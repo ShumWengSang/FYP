b2f5b18563cf325c25d696af6120f1539d4479a0
ShumWengSang <ballwallcallall@gmail.com>
2015-06-04 18:10:58 +0800

    Added A* pathfinding

---
+++ FYP Unity Project/Assets/Scripts/TestingPlayerPrefs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingPlayerPrefs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (PlayerPrefs.HasKey("Testing"))
+        {
+            Debug.Log("I have key for player prefs");
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("Testing", 1);
+        }
+        PlayerPrefs.Save();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ FYP Unity Project/Assets/UpDown.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDown : MonoBehaviour {
+
+    private Transform theTransform;
+	// Use this for initialization
+	void Start () {
+        theTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theTransform.position += new Vector3(0, 1, 0) * Time.deltaTime;
+	}
+}
