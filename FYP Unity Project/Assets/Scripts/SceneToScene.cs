98a347c2049038de312fdae765fefbfb14bd5532
crystalweiii <unified_lighterX@hotmail.com>
2015-06-04 18:06:21 +0800

    Transition/Folder Maker

---
+++ FYP Unity Project/Assets/Scripts/SceneToScene.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+//using System.
+
+using System.Collections;
+
+
+public enum SceneName
+{
+	SceneName_Null,
+	SceneName_MainMenu,
+	SceneName_GameScene
+}
+
+
+public class SceneToScene : MonoBehaviour {
+	const string MainMenu = "Main Menu";
+	const string GameScene = "Game Scene";
+
+	public SceneName sceneToChangeTo = SceneName.SceneName_Null;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void _ToMainMenu()
+	{
+		sceneToChangeTo = SceneName.SceneName_MainMenu;
+		ChangeScene ();
+		Debug.Log ("Changing to main menu");
+	}
+
+	public void _ToGameScene()
+	{
+		sceneToChangeTo = SceneName.SceneName_GameScene;
+		ChangeScene ();
+		Debug.Log ("Changing to game scene");
+	}
+
+	void ChangeScene()
+	{
+		//Check for scene to be changed to
+		if (sceneToChangeTo != SceneName.SceneName_Null) 
+		{
+			switch(sceneToChangeTo)
+			{
+			case SceneName.SceneName_MainMenu:
+			{
+				Application.LoadLevel(MainMenu);
+				break;
+			}
+			case SceneName.SceneName_GameScene:
+			{
+				Application.LoadLevel(GameScene);
+				break;
+			}
+				
+			}
+			sceneToChangeTo = SceneName.SceneName_Null;
+		}
+	}
+}
