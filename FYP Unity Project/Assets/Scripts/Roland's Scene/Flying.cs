16a7d1b9894ffbf3d5e68977bc167d5dec061483
ShumWengSang <ballwallcallall@gmail.com>
2015-06-05 14:19:55 +0800

    Added InControl scripts.
    More control over controllers now. Camera movement possible.

---
+++ FYP Unity Project/Assets/Scripts/Roland's Scene/Flying.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flying : MonoBehaviour {
+
+    public bool x = false;
+	// Use this for initialization
+	void Start () {
+        x = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ FYP Unity Project/Assets/Scripts/Roland's Scene/RolandCamMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+public class RolandCamMove : MonoBehaviour
+{
+    InputControl control;
+    InputDevice device;
+    public Transform targetObject;
+    // Use this for initialization
+    void Start()
+    {
+        device = InputManager.ActiveDevice;
+        control = device.GetControl(InputControlType.Action1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.ActiveDevice.Action1.WasPressed)
+        {
+            targetObject.transform.position += new Vector3(0, 1, 0) * Time.fixedDeltaTime;
+        }
+
+        var inputDevice = InputManager.ActiveDevice;
+
+        // Move focus with directional inputs.
+        if (inputDevice.Direction.Up.WasPressed)
+            transform.position += new Vector3(0, 0, 1);
+
+        if (inputDevice.Direction.Down.WasPressed)
+            transform.position += new Vector3(0, 0, -1);
+
+        if (inputDevice.Direction.Left.WasPressed)
+            transform.position += new Vector3(-1, 0, 0);
+
+        if (inputDevice.Direction.Right.WasPressed)
+            transform.position += new Vector3(1, 0, 0);
+
+    }
+}
